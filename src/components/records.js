3d0317aeff7a1e163290a70ccf15ca79773fb700
ryu-yatish <43433644+ryu-yatish@users.noreply.github.com>
2021-04-08 19:07:11 +0530

    record details added

--- src/components/records.js
+++ src/components/records.js
@@ -23,14 +23,15 @@ const Records = () => {
   const [land, setLand] = useState(false)
   const [year, setYear] = useState(0)
   const [data, setData] = useState([]);
-  const [loading, setLoading] =useState(false)
+  const [loading, setLoading] =useState(true)
+  const [selectedRow, setSelectedRow] = useState(null);
 
   const fetchdata = (url) => {
-  
+      setLoading(true)
       fetch(url)
       .then(response => response.json())
       .then((json) => setData(json));
-  
+      setLoading(false)
   }
   useEffect(async ()=>{
     fetchdata(query)
@@ -74,11 +75,23 @@ const Records = () => {
     console.warn(query)
     
   }
+  function jsonToSCSS(stringdata=``, data={}) {
+    /* JSON.parse can throw. Always be ready for that. */
+    try { data = JSON.parse(stringdata); }
+    catch (e) { console.warn(e); return ``; }
+  
+    return Object.keys(data)
+                 .map(key => `${key}: ${data[key]};`)
+                 .join('\n');
+  }
+  
   return (
     
     <div>
+      <br></br>
       {
         loading ? <div>...loading</div> :
+
         <div>
           <div>
             <FormControl component="fieldset">
@@ -95,14 +108,12 @@ const Records = () => {
               label="landing successful"
               labelPlacement="start"
               />
-              
-              <FormControl >
-              
+              <div>     </div>
               <Select value={year}
               displayEmpty
               onChange={updateSelectedVal}
               >
-                <MenuItem value={0}>None</MenuItem>
+                <MenuItem value={0}>  year  </MenuItem>
                 <MenuItem value={2005}>2005</MenuItem>
                 <MenuItem value={2006}>2006</MenuItem>
                 <MenuItem value={2007}>2007</MenuItem>
@@ -121,7 +132,7 @@ const Records = () => {
                 <MenuItem value={2020}>2020</MenuItem>
                 <MenuItem value={2021}>2021</MenuItem>
                 </Select>
-                </FormControl>
+               
             
               </FormGroup>
             </FormControl>
@@ -130,16 +141,41 @@ const Records = () => {
             </Button>
             </div>
             <div>
+            
             <MaterialTable style={{marginLeft:'10px', marginRight:'10px'}}
-              title="Spacex Launch Records"
+              title=""
               columns={[
                 { title: 'Flight number', field: 'flight_number'},
-                { title: 'Mission name', field: 'mission_name'},
-                { title: 'Mission ID', field: 'mission_id'},
-                { title: 'Launch year', field: 'launch_year'},
+                { title: 'Mission name', field: 'mission_name' },
+                { title: 'Launch year', field: 'launch_year', type :' numeric'},
                 
               ]}
               data = {data}
+              detailPanel={[
+                {
+                  tooltip: 'details',
+                  render: rowData => {
+                    return (
+                      <div>
+                        Flight number: {rowData.flight_number} <br></br> mission id: {rowData.mission_id} <br></br> 
+                        Missio nname: {rowData.mission_name} <br></br> upcoming: {rowData.upcoming?'true':'false'}  <br></br> 
+                        launch_year: {rowData.launch_year} <br></br> launch_date_unix: {rowData.launch_date_unix}  <br></br> 
+                        launch_date_utc: {rowData.launch_date_utc} <br></br> launch_date_local: {rowData.launch_date_local}  <br></br> 
+                        is_tentative: {rowData.is_tentative?'true':'false'} <br></br> tentative_max_precision: {rowData.tentative_max_precision}   <br></br>  
+                        tbd: {rowData.tbd?'true':'false'} <br></br> launch_window: {rowData.launch_window}  <br></br> 
+                        rocket: {JSON.stringify(rowData.rocket,null,2)} <br></br> ships: {JSON.stringify(rowData.ships,null, 2)} <br></br>  
+                        telemetry: {JSON.stringify(rowData.telemetry,null,2)} <br></br> launch_site: {JSON.stringify(rowData.launch_site,null, 2)} <br></br>  
+                        launch_success: {rowData.launch_success?'true':'false'} <br></br> launch_failure_details: {JSON.stringify(rowData.launch_failure_details,null, 2)}  <br></br> 
+                        links: {JSON.stringify(rowData.links,null,2)} <br></br> details: {rowData.details}  <br></br> 
+                        static_fire_date_utc: {rowData.static_fire_date_utc} <br></br> static_fire_date_unix: {rowData.static_fire_date_unix}  <br></br> 
+                        timeline: {JSON.stringify(rowData.timeline,null,2)} <br></br> crew: {rowData.crew}  <br></br> 
+                        tableData: {JSON.stringify(rowData.tableData,null,2)} <br></br><br></br> 
+                      </div>
+                    )
+                  },
+                }
+              ]}
+              onRowClick={(event, rowData, togglePanel) => togglePanel()}
               actions={[
                 {
                   icon: 'refresh',
@@ -148,16 +184,20 @@ const Records = () => {
                   onClick: () => this.tableRef.current && this.tableRef.current.onQueryChange(),
                 }, 
               ]}
+             
               options={{
                 
-                headerStyle: {
-                  backgroundColor: '#3f51b5',
-                  color: '#FFFF'
-                }}
-              }
-              
+               grouping: false,
+               filtering: true,
+               headerStyle: {
+                backgroundColor: '#01579b',
+                color: '#FFF'
+               },
+               
+              }}
             />
             <br/><br/><br/>
+          
             </div>
             
         </div>
